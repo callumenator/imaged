dc0ea98eacd122d7ca7326620edda1ba5e064266
ponce <aliloko@gmail.com>
2014-08-05 23:24:48 +0200

    Add package.d, now able to use "import imaged".

---
+++ imaged/package.d
@@ -0,0 +1,14 @@
+// Written in the D programming language.
+
+/**
+* Copyright: Copyright 2014 -
+* License: $(WEB www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+* Authors: Callum Anderson, Stewart Gordon
+* Date: August 5, 2014
+*/
+module imaged;
+
+public import
+    imaged.image,
+    imaged.jpeg,
+    imaged.png;
